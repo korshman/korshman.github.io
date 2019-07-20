96db6e64bd2688eea5bb16a37c73aa2b3bc33d4c
Ikor-ishor Samuel <leumas4u@gmail.com>
2019-07-20 16:53:39 +0100

    changes to navbar
    changes to nav bar

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    var y = document.getElementById('navbarsExampleDefault');
+
+    y.onclick = function(){
+    document.getElementById('navbarsExampleDefault').className = 'navbar-collapse collapse hide';};
+</script>
